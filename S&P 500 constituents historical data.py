775a69001ec26f8726fd5ad7087a7050979f9b71
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-09-29 23:23:50 +0200

    Update S&P 500 constituents historical data.py

--- S&P 500 constituents historical data.py
+++ S&P 500 constituents historical data.py
@@ -20,10 +20,23 @@ def get_stock_data():
     tables = pd.read_html(response.text)
     sp500_tickers = tables[0]["Symbol"].tolist()
     # Some tickers have dots in them which yfinance does not recognize, replace them with hyphens
-    data = yf.download(sp500_tickers, start='2000-01-01', end=date.datetime.today(), group_by='ticker')
-    data = data.dropna(how='all')  # Drop rows where all elements are NaN
-    return data
+    data_constituents = yf.download(sp500_tickers, start='2000-01-01', end=date.datetime.today(), group_by='ticker')
+    data_constituents = data_constituents.dropna(how='all')  # Drop rows where all elements are NaN
+    return data_constituents
 
-print(get_stock_data().head())
+SP500_constituents = get_stock_data()
+print(SP500_constituents["MSFT"].loc["2025"].head())
 
+# Calculate returns 
+def calculate_returns(data):
+    returns = pd.DataFrame()
+    for ticker in data.columns.levels[0]:
+        returns[ticker] = data[ticker]['Close'].pct_change()
+    returns = returns.dropna()
+    return returns
 
+# 
+# Calculate covariance matrix
+def calculate_covariance(returns):
+    covariance_matrix = returns.cov() * 252  # Annualize the covariance matrix
+    return covariance_matrix
