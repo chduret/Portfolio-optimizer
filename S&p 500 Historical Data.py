d5dc3fe99ce3eadc7aa8881d8197a90afdfe20cc
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-09-29 18:02:07 +0200

    Update S&p 500 Historical Data.py

--- S&p 500 Historical Data.py
+++ S&p 500 Historical Data.py
@@ -5,30 +5,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 import datetime as date
+import scipy.stats as stats
 
 # Download historical data for S&P 500
 data = yf.download('^GSPC', start='2000-01-01', end=date.datetime.today())
+# Dowload risk free rate data (3-Month Treasury Bill and 10-Year Treasury Note)
+risk_free_3m = yf.download('^IRX', start='2000-01-01', end=date.datetime.today())
+risk_free_10 = yf.download('^TNX', start='2000-01-01', end=date.datetime.today())
+
+# Closing prices
 data = data['Close']
 data = data.dropna()
+
+# Convert to DataFrame
 data = pd.DataFrame(data)
 data.columns = ['Close']
+
+# Daily returns
 data['Return'] = data['Close'].pct_change()
 data = data.dropna()
+# Log returns
 data['LogReturn'] = np.log(data['Close'] / data['Close'].shift(1))
 data = data.dropna()
 #Daily volatility
 data['Daily_vol'] = data['LogReturn'].rolling(window=21).std()
+# Annualized volatility
 data['Volatility'] = data['LogReturn'].rolling(window=21).std() * np.sqrt(252)  
 data = data.dropna()
+# Cumulative returns
 data['CumulativeReturn'] = (1 + data['Return']).cumprod() - 1
 data = data.dropna()
+# Cumulative log returns
 data['CumulativeLogReturn'] = data['LogReturn'].cumsum()
 data = data.dropna() 
-print(data.head())
+
+# risk free rate
+data['RiskFree_3M'] = risk_free_3m['Close'] / 100
+data['RiskFree_10Y'] = risk_free_10['Close'] / 100
+data = data.dropna()
+print(data[["RiskFree_3M" , "RiskFree_10Y"]].head())
 
 """
 We now have the close prices, daily returns, log returns, daily volatility, 
 annualized volatility, cumulative returns, 
 and cumulative log returns for the S&P 500 index from January 1, 2020, to January 1, 2023.
 """
-print(data["2025-01-01":"2025-09-29"])
+# Calculate expected return 
+# Historical Expected Return
+data['Historical ER'] = data['LogReturn'].mean() * 252
+# Implied Expected Return with CAPM
