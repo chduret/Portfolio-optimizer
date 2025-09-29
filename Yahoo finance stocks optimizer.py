d78b529b5cc534a09f3e70e2e03b5f314417967b
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-09-30 00:48:37 +0200

    Update Yahoo finance stocks optimizer.py

--- Yahoo finance stocks optimizer.py
+++ Yahoo finance stocks optimizer.py
@@ -15,7 +15,23 @@ def get_stock_data(*tickers):
     
     return data
 
+#Test
 print(get_stock_data("AAPL", "GOOG", "MC.PA").tail())
 
+def ann_ret(data):
+    daily_returns = data['Adj Close'].pct_change().dropna()
+    mean_daily_returns = daily_returns.mean()
+    annualized_return = (1 + mean_daily_returns) ** 252 - 1
+    return annualized_return
 
-    
+def ann_vol(data):
+    daily_returns = data['Adj Close'].pct_change().dropna()
+    daily_volatility = daily_returns.std()
+    annualized_volatility = daily_volatility * np.sqrt(252)
+    return annualized_volatility
+
+def daily_vol(data):
+    daily_returns = data['Adj Close'].pct_change().dropna()
+    daily_volatility = daily_returns.std()
+    return daily_volatility
+
