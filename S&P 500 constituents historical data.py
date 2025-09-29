4e38f283bc938670184e39b9aaaf629bb426bd85
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-09-29 23:27:17 +0200

    Update S&P 500 constituents historical data.py

--- S&P 500 constituents historical data.py
+++ S&P 500 constituents historical data.py
@@ -20,12 +20,13 @@ def get_stock_data():
     tables = pd.read_html(response.text)
     sp500_tickers = tables[0]["Symbol"].tolist()
     # Some tickers have dots in them which yfinance does not recognize, replace them with hyphens
-    data_constituents = yf.download(sp500_tickers, start='2000-01-01', end=date.datetime.today(), group_by='ticker')
-    data_constituents = data_constituents.dropna(how='all')  # Drop rows where all elements are NaN
-    return data_constituents
+    data = yf.download(sp500_tickers, start='2000-01-01', end=date.datetime.today(), group_by='ticker')
+    data = data.dropna(how='all')  # Drop rows where all elements are NaN
+    return data
 
-SP500_constituents = get_stock_data()
-print(SP500_constituents["MSFT"].loc["2025"].head())
+# test
+# SP500_constituents = get_stock_data()
+# print(SP500_constituents["MSFT"].loc["2025"].head())
 
 # Calculate returns 
 def calculate_returns(data):
@@ -35,7 +36,14 @@ def calculate_returns(data):
     returns = returns.dropna()
     return returns
 
-# 
+# calculate volatility
+def calculate_volatility(returns):
+    ## Daily volatility
+    ### Rolling window is set to 21 days (approximately one month - can be 3 months or 1 year: window=252)
+    Dayly_vol = returns.rolling(window=21).std()
+    Ann_vol = returns.rolling(window=21).std() * np.sqrt(252)  # Annualized volatility
+    return volatility
+
 # Calculate covariance matrix
 def calculate_covariance(returns):
     covariance_matrix = returns.cov() * 252  # Annualize the covariance matrix
