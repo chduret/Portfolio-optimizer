355ab0ae57889da23621a10a9a91978460bb8fd5
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-10-27 13:30:05 +0100

    rearrange

--- test/test.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-user_input = input("Entrez les tickers (séparés par des virgules) : ")
-tickers = [t.strip() for t in user_input.split(",") if t.strip()]
-
-# Télécharger les données
-data = yf.download(tickers, start="2020-01-01")["Close"]
-print(data.tail())
